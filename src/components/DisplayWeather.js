0cb2ede1e045d85af14fe8ba1230de312e787590
Jn-Sajol <julkar10121@gmail.com>
2023-08-20 23:38:29 +0600

    2nd one

--- src/components/DisplayWeather.js
+++ src/components/DisplayWeather.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./displayweather.css";
 function DisplayWeather(props) {
   const { data } = props;
+  console.log(data)
   const iconurl =
     "http://openweathermap.org/img/wn/" +
     `${data.cod !== 404 ? data.weather[0].icon : null}` +
